e698b5e74b2a6b183468cdd09eca2f1b897d71bd
sikorskayaX <sikorskaya02@gmail.com>
2023-10-23 00:35:01 +0300

    Add User Post Functionality
    Implemented a feature that allows users to create and save posts on their profile. This includes a form for inputting the post content and a button for submitting the post. The post data is then stored in the database with relevant information such as the user's ID, the post content, and the time of posting.
    
    Additionally, the user's posts are retrieved from the database and displayed on their profile. The posts are displayed in the order they were created, with the most recent post appearing first.
    
    Minor changes were also made to the profile layout, including the addition of an "edit profile" button for users viewing their own profile, and updates to the navigation links.

--- user.php
+++ user.php
@@ -2,6 +2,8 @@
 
 require "db.php";
 
+$data = $_POST;
+
 if($_GET['id'] == ''){
     header('Location: /user?id='.$_SESSION['login_user']->id);
 }
@@ -17,6 +19,32 @@ else{
 
 $user = R::findOne('users', 'id = ?', array($_GET['id']));
 
+
+if(isset($data["send_post"])) { //sign up user
+    $post = $data['post'];
+    if($post){
+        $db_post = R::dispense('posts');
+        $db_post->id_user = $_SESSION['login_user']->id; 
+        $db_post->post = $post;
+        $db_post->ip = $_SERVER['REMOTE_ADDR'];
+        $db_post->d_date_post = date('d');
+        $db_post->m_date_post = date('m');
+        $db_post->y_date_post = date('Y');
+        $db_post->h_time_post = date('H');
+        $db_post->m_time_post = date('i');
+
+    }
+    R::store($db_post);
+}
+
+$all_post = R::findAll('posts');
+$user_posts = array();
+
+foreach($all_post as $row){
+    if($row['id_user'] == $_GET['id']){
+        $user_posts[] = $row;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -29,14 +57,33 @@ $user = R::findOne('users', 'id = ?', array($_GET['id']));
 </head>
 <body>
     <h1><?php echo $user->firstname." ".$user->lastname; ?></h1>
+    <div>
+        <form action = "user?id = <?php echo $_GET['id']; ?>" method="POST">
+            <input type="text" name = "post" placeholder=" write your post" >
+            <button type="submit" name = "send_post">save</button>
+
+        </form>
+    </div>
+
+    <?php for($i = 0; $i < count($user_posts); $i++) : ?>
+        <div>
+            <p>
+                <?php echo htmlspecialchars($user_posts[$i]['post']); ?>
+            </p>
+        </div>
+    <?php endfor; ?>
 
     <?php if($position == 'view') :?>
         <div>
             <button>write message</button>
             <button>add to friends</button>
         </div>
+    <?php else: ?>
+        <button>edit profile</button> 
     <?php endif; ?>
-
-    <a href="/logout.php">Log out</a>
+    <br>
+    <a href="/logout">log out</a>
+    <br>
+    <a href="/user">my page</a>
 </body>
 </html>
